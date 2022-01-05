f1d20f227dfdeb8b043eeb3f93b175a8ba7d5d83
Abdul Samad <81066980+Abdulsamad27@users.noreply.github.com>
2022-01-05 19:02:45 +0530

    Update fake news detection1.py

--- fake news detection1.py
+++ fake news detection1.py
@@ -45,7 +45,7 @@ x_train,x_test,y_train,y_test=train_test_split(variable['text'], labels, test_si
 #Dataset - Initialize a TfidfVectorizer
 tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.7)
 
-#DataFlair - Fit and transform train set, transform test set
+#Dataset - Fit and transform train set, transform test set
 tfidf_train=tfidf_vectorizer.fit_transform(x_train) 
 tfidf_test=tfidf_vectorizer.transform(x_test)
 
