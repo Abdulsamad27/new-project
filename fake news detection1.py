b92849d560208cf3ba2dc88cab9b7db5e83a5394
Abdul Samad <81066980+Abdulsamad27@users.noreply.github.com>
2022-01-13 12:48:03 +0530

    Update fake news detection1.py

--- fake news detection1.py
+++ fake news detection1.py
@@ -7,10 +7,13 @@
 import numpy as np
 import pandas as pd
 import itertools
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import plot_confusion_matrix_matrix
 
 
 # In[3]:
